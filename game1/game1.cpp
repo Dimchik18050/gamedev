be964f4e6c22f9f6f4ffe4ebc210db94b80c752d
Dmytro Khomenko <dimitriy.khomenko@ukr.net>
2023-03-31 18:04:03 +0300

    A random number generator has been created, func printf has been added to display information on the screen.

--- game1/game1.cpp
+++ game1/game1.cpp
@@ -2,12 +2,21 @@
 // Classic number guessing game
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void main()
 {
 	const int numberRange = 100;
 	int computerNumber = 0;
 	int playerNumber = 0;
+	
+	time_t currentTime = time(0);
+	
+	srand( currentTime );
+	computerNumber = rand();
+	
+	printf("Computer set number: %i Range: %i to %i \n", computerNumber, 0, numberRange);
 	
 	
 }
