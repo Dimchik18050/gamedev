6bebb8648a9a715cba3c36b467d4d9dc7b5993b3
Dmytro Khomenko <dimitriy.khomenko@ukr.net>
2023-03-31 17:26:33 +0300

    Created 2 variables - the secret number computer, the number entered by the player and a constant - the limit of the hidden number.

---
+++ game1/game1.cpp
@@ -0,0 +1,13 @@
+// Guess My Number
+// Classic number guessing game
+
+#include <stdio.h>
+
+void main()
+{
+	const int numberRange = 100;
+	int computerNumber = 0;
+	int playerNumber = 0;
+	
+	
+}
